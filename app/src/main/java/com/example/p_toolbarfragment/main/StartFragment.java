88e8e9aa0cd185dcf9e656929068b56b657f1920
jennifer <gorillaz1801058@gmail.com>
2020-08-13 22:32:23 +0800

    v1

--- app/src/main/java/com/example/p_toolbarfragment/main/StartFragment.java
+++ app/src/main/java/com/example/p_toolbarfragment/main/StartFragment.java
@@ -43,7 +43,7 @@ public class StartFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        EventBus.getDefault().register(getActivity());
+
 
     }
 
